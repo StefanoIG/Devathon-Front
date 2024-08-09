4f1f84ce5b645f751317158aa7fb754ff1d91178
MiltonCoronado <miltonext@hotmail.com>
2024-08-09 11:53:32 -0500

    fix/jsx and css more responsive

--- Hero/Hero.jsx
+++ Hero/Hero.jsx
@@ -5,12 +5,11 @@ const Hero = () => {
     <header className='hero'>
       <div className='hero-content'>
         <h1>MexaCol</h1>
-          <p>Donde la riqueza de México y Colombia se encuentran en tu plato.</p>
-          <a href='#' className='cta-button'>Reserva tu Mesa</a>
+        <p>Donde la riqueza de México y Colombia se encuentran en tu plato.</p>
+        <a href='#' className='cta-button'>Reserva tu Mesa</a>
       </div>
     </header>
   );
 };
 
 export { Hero };
-
