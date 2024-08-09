3144de33c67c9f98f806b391bf94fd5a60bfa226
MiltonCoronado <miltonext@hotmail.com>
2024-08-09 16:22:31 -0500

    feat/Footer

---
+++ Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <p>&copy; 2024 Sabores Latinos. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export { Footer };
