0b6d814a8cfa74e746b51d82579aae61c88bf80f
MiltonCoronado <miltonext@hotmail.com>
2024-08-09 14:29:44 -0500

    feat/AboutUs component added

---
+++ AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import './AboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <section className='about-us'>
+      <h2>Nuestra Cultura</h2>
+      <p>En Sabores Latinos, celebramos la riqueza cultural y gastronómica de México y Colombia. Nuestros platillos son una mezcla de tradición y sabor, utilizando ingredientes frescos y auténticos que evocan los aromas y colores de nuestras tierras.</p>
+    </section>
+  );
+};
+
+export { AboutUs };
