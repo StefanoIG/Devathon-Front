6c8d31d7296c9ad6f8be8be9fdecd1ddcc739064
MiltonCoronado <miltonext@hotmail.com>
2024-08-14 16:22:54 -0500

    feat/integration_views_landing

---
+++ landing/src/App.jsx
@@ -0,0 +1,17 @@
+import { Hero } from './Hero/Hero.jsx';
+import { AboutUs } from './AboutUs/AboutUs.jsx';
+import { Menu } from './Menu/Menu.jsx';
+import { Footer } from './Footer/Footer.jsx';
+
+const App =  () => {
+  return (
+    <>
+      <Hero />
+      <AboutUs />
+      <Menu />
+      <Footer />
+    </>
+  )
+};
+
+export { App };
